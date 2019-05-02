2fe2670f0297e14ceada4cefede187ff6cc2551b
Pongsasit Thongpramoon <48168818+Pongsasit@users.noreply.github.com>
2019-05-03 00:59:54 +0900

    Add files via upload

---
+++ linear_regression/lahman.R
@@ -0,0 +1,8 @@
+library(Lahman)
+library(tidyverse)
+
+#whether teams with more at-bats per game have more runs per game
+Teams %>% filter(yearID %in% 1961:2001 ) %>%
+  mutate(AB_per_game = AB/G, R_per_game = R/G) %>%
+  ggplot(aes(AB_per_game, R_per_game)) + 
+  geom_point(alpha = 0.5)
